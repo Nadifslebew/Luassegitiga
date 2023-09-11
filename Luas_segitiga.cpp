99416ba962bb08646b3a941dce2bb73a41edc26e
Nadifslebew <144640443+Nadifslebew@users.noreply.github.com>
2023-09-11 09:44:35 +0700

    Create Luas_segitiga.cpp

---
+++ Luas_segitiga.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, t, L;
+    cout<<"MENGHITUNG LUAS SEGITIGA \n";
+    cout<<"Masukan Alas: ";
+    cin>>a;
+    cout<<"Masukan Tinggi: ";
+    cin>>t;
+    L=0.5*a*t;
+    cout<<"hasil adalah: "<<L<<endl;
+}
